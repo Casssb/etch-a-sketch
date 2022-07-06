4b8b68f15b9fdb707ae03e5f8e5c3d81056bad92
Joe <caspar0912@googlemail.com>
2022-07-06 21:27:29 +0100

    updated ui added buttons

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+let gridSizeSlider = document.querySelector("#grid-size")
+let gridSize = gridSizeSlider.value
+let grid = document.querySelector(".grid-container");
+
 
 
 function makeGrid() {
-  let grid = document.querySelector(".grid-container");
   let box = document.createElement("div");
-  box.style.border = "0.1rem solid black";
+  box.style.border = '0.5px solid black';
   box.style.width = "auto";
   box.style.height = "auto";
   grid.appendChild(box);
 };
-let count = 4096;
 
-for(let i = 0; i < count; i++) {
+grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+for(let i = 0; i < gridSize * gridSize; i++) {
   makeGrid()
 }
